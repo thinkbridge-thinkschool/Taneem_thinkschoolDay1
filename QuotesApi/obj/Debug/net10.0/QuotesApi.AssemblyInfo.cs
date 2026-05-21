525db1f42d07f2d5c2c305d099a1e9d122c0948b
Taneem110 <sajjantaneem19@gmail.com>
2026-05-21 09:58:02 +0530

    Added new QuotesAPi

--- QuotesApi/obj/Debug/net10.0/QuotesApi.AssemblyInfo.cs
+++ QuotesApi/obj/Debug/net10.0/QuotesApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f9c18184f07131151aa34854afb39f0f202d66e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
